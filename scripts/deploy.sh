336bb4d862ed9f37e08bd3fe9d52c4535c3a4feb
Lucas Ewalt <lucasewalt@Lucass-MacBook-Air.local>
2018-07-15 00:29:21 -0400

    fix spacing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ else
 	docker tag bind dnstraining.azurecr.io/bind:$1
 	docker push dnstraining.azurecr.io/bind:$1
 
-	# deploy new docker container
+  # deploy new docker container
   kubectl set image deployment/ns1-deployment ns1=dnstraining.azurecr.io/bind:$1
   kubectl set image deployment/ns2-deployment ns2=dnstraining.azurecr.io/bind:$1
 fi
